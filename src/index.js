afc4b079ca06fe4a43305a66cf9a9a83957a58fd
Raj Sagar <therajsagar@gmail.com>
2019-01-09 23:49:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const App = ()=> <h1>Meme Generator</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'));
